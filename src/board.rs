db27ffe3f93fda9518f911adeed51aabb0fe1a7d
Takuya Sekikawa <konao235@gmail.com>
2021-08-29 22:24:54 +0900

    Pos::idxが返り値をOptionで返すように修正

--- src/board.rs
+++ src/board.rs
@@ -19,8 +19,12 @@ impl Pos {
         return ((self.y-1) * 8 + (self.x-1)).into();
     }
 
-    pub fn idx(x: i32, y: i32) -> usize {
-        return ((y-1) * 8 + (x-1)) as usize;
+    pub fn idx(x: i32, y: i32) -> Option<usize> {
+        if (x >= 1) && (x <= 8) && (y >= 1) && (y <= 8) {
+            return Some(((y-1) * 8 + (x-1)) as usize);
+        } else {
+            return None;
+        }
     }
 }
 
@@ -48,14 +52,15 @@ impl Board {
     pub fn print(&self) -> () {
         for y in 1..=8 {
             for x in 1..=8 {
-                let idx = Pos::idx(x, y);
-                let piece = &self.pieces[idx];
-                let pstr = match piece {
-                    Piece::White => "○",
-                    Piece::Black => "●",
-                    _ => "・"
-                };
-                print!("{}", pstr);
+                if let Some(idx) = Pos::idx(x, y) {
+                    let piece = &self.pieces[idx];
+                    let pstr = match piece {
+                        Piece::White => "○",
+                        Piece::Black => "●",
+                        _ => "・"
+                    };
+                    print!("{}", pstr);    
+                }
             }
             println!();
         }
@@ -63,19 +68,28 @@ impl Board {
 
     // 初期状態にする
     pub fn init(&mut self) -> () {
-        self.pieces[Pos::idx(4, 4)] = Piece::White;
-        self.pieces[Pos::idx(5, 5)] = Piece::White;
-        self.pieces[Pos::idx(4, 5)] = Piece::Black;
-        self.pieces[Pos::idx(5, 4)] = Piece::Black;
+        self.setPiece(4, 4, Piece::White);
+        self.setPiece(5, 5, Piece::White);
+        self.setPiece(4, 5, Piece::Black);
+        self.setPiece(5, 4, Piece::Black);
     }
 
-    pub fn getPiece(&self, p: Pos) -> &Piece {
-        let ind = p.toIdx();  // 位置から（配列の）インデックスへ変換
-        return &self.pieces[ind];
+    pub fn setPiece(&mut self, x: i32, y: i32, piece: Piece) -> () {
+        if let Some(idx) = Pos::idx(x, y) {
+            self.pieces[idx] = piece;
+        }
+    }
+
+    pub fn getPiece(&self, x: i32, y: i32) -> Option<&Piece> {
+        if let Some(idx) = Pos::idx(x, y) {
+            return Some(&self.pieces[idx]);
+        } else {
+            return None;
+        }
     }
 
     // pidが次に置ける場所を探す
-    pub fn searchPos(&self, pid: Piece) -> Vec<Pos> {
+    pub fn searchPos(&self, piece: Piece) -> Vec<Pos> {
         return Vec::new();
     }
 
