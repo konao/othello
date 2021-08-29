0fb23d26d9279f5928af07fcd3c55e75b300eaba
Takuya Sekikawa <konao235@gmail.com>
2021-08-30 01:14:04 +0900

    searchPos実装追加

--- src/board.rs
+++ src/board.rs
@@ -1,11 +1,23 @@
 #![allow(non_snake_case)]
 
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Piece {
     Space,
     White,
     Black
 }
 
+impl Piece {
+    pub fn getOpponent(piece: &Piece) -> Piece {
+        return match *piece {
+            Piece::Space => Piece::Space,
+            Piece::White => Piece::Black,
+            Piece::Black => Piece::White
+        };
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Pos {
     x: u8,  // 1..8
     y: u8   // 1..8
@@ -15,9 +27,9 @@ impl Pos {
     // Pos構造体からBoardのインデックスを返す
     //
     // (ex)
-    pub fn toIdx(&self) -> usize {
-        return ((self.y-1) * 8 + (self.x-1)).into();
-    }
+    // pub fn toIdx(&self) -> usize {
+    //     return ((self.y-1) * 8 + (self.x-1)).into();
+    // }
 
     pub fn idx(x: i32, y: i32) -> Option<usize> {
         if (x >= 1) && (x <= 8) && (y >= 1) && (y <= 8) {
@@ -26,6 +38,27 @@ impl Pos {
             return None;
         }
     }
+
+    pub fn getDxDy(dir: i32) -> (i32, i32) {
+        return match dir {
+            0 => (0, -1),
+            1 => (1, -1),
+            2 => (1, 0),
+            3 => (1, 1),
+            4 => (0, 1),
+            5 => (-1, 1),
+            6 => (-1, 0),
+            7 => (-1, -1),
+            _ => (0, 0)
+        };
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct PlaceInfo {
+    pub x: i32,
+    pub y: i32,
+    pub score: i32
 }
 
 pub struct Board {
@@ -89,8 +122,74 @@ impl Board {
     }
 
     // pidが次に置ける場所を探す
-    pub fn searchPos(&self, piece: Piece) -> Vec<Pos> {
-        return Vec::new();
+    pub fn searchPos(&self, piece: &Piece) -> Vec<PlaceInfo> {
+        let mut result = Vec::new();
+        for y in 1..=8 {
+            for x in 1..=8 {
+                let score = self.searchPosSub(piece, x, y);
+                if score>0 {
+                    let info = PlaceInfo {
+                        x: x,
+                        y: y,
+                        score: score
+                    };
+                    result.push(info);
+                }
+            }
+        }
+        return result;
+    }
+
+    // (x, y)にpieceを置いたら何個相手の駒を取れるかを返す
+    // @param x [i]
+    // @param y [i]
+    pub fn searchPosSub(&self, piece: &Piece, x: i32, y: i32) -> i32 {
+        let opponent = Piece::getOpponent(piece);
+        let mut score = 0;
+        for dir in 0..8 {
+            let (dx, dy) = Pos::getDxDy(dir);
+            let mut x1 = x + dx;
+            let mut y1 = y + dy;
+            if let Some(p) = self.getPiece(x1, y1) {
+                if *p == opponent {
+                    // (dx, dy)に進めたら敵の駒があった
+                    // さらに進めて、空白になる前に自分の駒があれば、(x, y)にpieceを置ける
+                    let mut c = 1;
+                    x1 = x1 + dx;
+                    y1 = y1 + dy;
+                    let mut proceeding = false;
+                    let mut got = false;
+                    if let Some(q) = self.getPiece(x1, y1) {
+                        if *q == opponent {
+                            proceeding = true;  // 続いている
+                        }
+                        else if *q == *piece {
+                            got = true; // 囲んだ
+                        }
+                    }
+                    while proceeding {
+                        c = c + 1;
+                        x1 = x1 + dx;
+                        y1 = y1 + dy;
+                        proceeding = false;
+                        got = false;
+                        if let Some(q) = self.getPiece(x1, y1) {
+                            if *q == opponent {
+                                proceeding = true;
+                            }
+                            else if *q == *piece {
+                                got = true;
+                            }
+                        }
+                    }
+                    if got {
+                        score += c;
+                    }
+                }
+            }
+        }
+
+        return score;
     }
 
     // 駒pidを位置pに置く、
--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,10 @@ fn main() {
     let mut board = board::Board::new();
     board.init();
     board.print();
+
+    let result = board.searchPos(&board::Piece::White);
+    for info in result.iter() {
+        println!("({}, {}) = {}", info.x, info.y, info.score);
+    }
 }
+    
